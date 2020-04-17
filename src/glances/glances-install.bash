88726524ab522453750fd5889023ce0ac42f551d
Rob Frawley 2nd <rmf@src.run>
2020-04-16 21:25:01 -0400

    add python3, python3-pip, python3-virtualenv package dependencies to glances install script

--- src/glances/glances-install.bash
+++ src/glances/glances-install.bash
@@ -32,6 +32,59 @@ function get_self_dirpath() {
 }
 
 #
+# get available package version from cache
+#
+
+function get_cache_pkg_ver() {
+  local name="${1}"
+
+  apt show "${name}" 2> /dev/null \
+    | grep -oE '^Version: .+$' 2> /dev/null \
+    | cut -d' ' -f2 2> /dev/null
+}
+
+#
+# get installed package version
+#
+
+function get_local_pkg_ver() {
+  local name="${1}"
+
+  dpkg -s "${name}" 2> /dev/null \
+    | grep -E '^Version: .+$' 2> /dev/null \
+    | cut -d' ' -f2 2> /dev/null
+}
+
+#
+# install passed packages (if not already)
+#
+
+function apt_install_packages() {
+    local -a package_names=("${@}")
+
+    printf -- 'Working to install %d packages using apt package manager ...\n' "${#package_names[@]}"
+
+    for p in "${package_names[@]}"; do
+      printf -- 'Working to install package "%s" (targeting version "%s") ... ' "${p}" "$(get_cache_pkg_ver "${p}")"
+
+      if dpkg -s "${p}" 2> /dev/null | grep installed &> /dev/null; then
+        printf -- '[skipped] (found version "%s")\n' "$(get_local_pkg_ver "${p}")"
+      else
+        export DEBIAN_FRONTEND=noninteractive
+
+        if sudo apt install "${p}" --assume-yes --quiet &> "${logger_path}"; then
+          printf -- '[success] (found version "%s")\n' "$(get_local_pkg_ver "${p}")"
+        else
+          printf -- '[failure]\nLogging file located at "%s" ... Exiting prematurely!\n' "${logger_path}"
+          exit 255
+        fi
+
+        unset DEBIAN_FRONTEND
+      fi
+    done
+}
+
+#
 # perform glances install operation
 #
 
@@ -58,6 +111,8 @@ function install() {
   [[ ! -f ${logger_path} ]] && touch "${logger_path}"
 
   printf -- 'Logging installation to "%s" (tail this file for additional command output) ...\n' "${logger_path}"
+
+  apt_install_packages python3 python3-pip python3-virtualenv
 
   printf -- 'Working to create virtual environment for "glances" using python (version %s) interpreter ... ' "$(python3 --version 2>&1 | cut -d' ' -f2)"
 
